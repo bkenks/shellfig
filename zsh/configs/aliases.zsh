6524c79adb05d34089a7788b99e025e346b8e6e0
Brian Kenkel <briankenkel.t@gmail.com>
2026-02-19 20:16:46 -0500

    add color

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -1,4 +1,16 @@
 alias c='clear'
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias lx="lazymux"
 alias reload="exec zsh"
 alias s="kitten ssh"
-alias lx="lazymux"
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias dir='dir --color=auto'
+    alias egrep='egrep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias grep='grep --color=auto'
+    alias ls='ls --color=auto'
+    alias vdir='vdir --color=auto'
+fi
